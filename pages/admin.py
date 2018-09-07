02b0c5104810d687fb22fbb3a5bfc99392b08980
Luis Bastidas <luisbastir@gmail.com>
2018-09-06 21:27:39 -0700

    crud done

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,11 @@ from .models import Page
 
 # Register your models here.
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'order')
+  list_display = ('title', 'order')
+
+  class Media:
+    css = {
+      "all": ('pages/css/custom_ckeditor.css',)
+    }
 
 admin.site.register(Page, PageAdmin)
